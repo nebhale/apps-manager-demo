b2fc43fe2846343f3612fc94db1aaa4312c4211a
Ben Hale <bhale@pivotal.io>
2017-10-16 13:50:08 -0700

    Spring Boot 2.x Update

--- src/main/java/io/pivotal/MutableHealthIndicator.java
+++ src/main/java/io/pivotal/MutableHealthIndicator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.pivotal;
 
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
@@ -11,19 +27,19 @@ import java.util.Map;
 @Component
 final class MutableHealthIndicator extends AbstractHealthIndicator {
 
-    private volatile Status status = Status.UP;
-
     private volatile Map<String, Object> details = Collections.emptyMap();
 
-    void mutate(Status status, Map<String, Object> details) {
-        this.status = status;
-        this.details = details;
-    }
+    private volatile Status status = Status.UP;
 
     @Override
     protected void doHealthCheck(Health.Builder builder) throws Exception {
         builder.status(this.status);
         this.details.forEach(builder::withDetail);
+    }
+
+    void mutate(Status status, Map<String, Object> details) {
+        this.status = status;
+        this.details = details;
     }
 
 }
--- src/main/java/io/pivotal/TestController.java
+++ src/main/java/io/pivotal/TestController.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.pivotal;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +27,11 @@ final class TestController {
 
     private static final Random RANDOM = new SecureRandom();
 
-    private final Timer timer;
-
-    TestController(MetricRegistry metricRegistry) {
-        this.timer = metricRegistry.timer("histogram.response.test");
-    }
-
     @GetMapping("/test")
     String test() throws InterruptedException {
-        try (Timer.Context context = this.timer.time()) {
-            int delay = RANDOM.nextInt(1_000);
-            Thread.sleep(delay);
-            return String.valueOf(delay);
-        }
+        int delay = RANDOM.nextInt(1_000);
+        Thread.sleep(delay);
+        return String.valueOf(delay);
     }
 
 }
