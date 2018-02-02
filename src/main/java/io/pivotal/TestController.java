27cf80a5e74f16fa2216635d0d48ab0e0abd49cc
Ben Hale <bhale@pivotal.io>
2018-02-02 11:22:05 -0800

    Remove Metrics

--- src/main/java/io/pivotal/TestController.java
+++ src/main/java/io/pivotal/TestController.java
@@ -1,7 +1,5 @@
 package io.pivotal;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +11,11 @@ final class TestController {
 
     private static final Random RANDOM = new SecureRandom();
 
-    private final Timer timer;
-
-    TestController(MetricRegistry metricRegistry) {
-        this.timer = metricRegistry.timer("histogram.response.test");
-    }
-
     @GetMapping("/test")
     String test() throws InterruptedException {
-        try (Timer.Context context = this.timer.time()) {
-            int delay = RANDOM.nextInt(1_000);
-            Thread.sleep(delay);
-            return String.valueOf(delay);
-        }
+        int delay = RANDOM.nextInt(1_000);
+        Thread.sleep(delay);
+        return String.valueOf(delay);
     }
 
 }
